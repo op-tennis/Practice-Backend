e4f8177813a74c585cac810aaa4ab5f97de43fda
takuma.shiraki <takuma.shiraki@optim.co.jp>
2024-04-29 12:21:43 +0900

    feat: #2 @swc/jestを導入 ・npm testを実行時にテストが失敗する ・ts-jestの時からソースコードに差分がほぼ無いのに落ちている理由がわからない

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+const fs = require('fs')
+const config = JSON.parse(fs.readFileSync(`${__dirname}/.swcrc`, 'utf-8'))
+
 module.exports = {
     preset: 'ts-jest',
+    // ts-jestから@swc/jestに変換
+    transform: {
+        '^.+\\.(t|j)sx?$': ['@swc/jest', { ...config, /* custom configuration in Jest */ }],
+    },
     testEnvironment: 'node',
 };
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 console.log("Hello, world!")
-export function sum(a:any, b:any
-) {
+export function sum(a:any, b:any) {
     return a + b;
 }
+// export const add = (a:any, b:any) => a + b;
+
