588cb2110c953be38a8e82fad67d3fba21c39f46
takuma.shiraki <takuma.shiraki@optim.co.jp>
2024-05-19 16:32:43 +0900

    update: #7 サーバーを立てる処理を追加

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,7 @@
-console.log("Hello, world!");
+import express from 'express'
 
-class Test {
-    // プロパティ
-    leftSide: number;
-    rightSide: number;
-
-    // コンストラクタ
-    constructor(leftSide: number, rightSide: number) {
-        this.leftSide = leftSide;
-        this.rightSide = rightSide;
-    }
-
-    // メソッド
-    logLeftSide() {
-        console.log(this.leftSide);
-    }
-
-    logRightSide() {
-        console.log(this.rightSide);
-    }
-}
-
-const calculation = new Test(1, 2);
-calculation.logLeftSide(); // 1
-calculation.logRightSide(); // 2
-
+// テスト用の処理
+// Serverのテストが完成したら削除する
 const a: number = 1
 const b: number = 2
 
@@ -32,3 +9,28 @@ export function sum(a: number, b: number) {
     return a + b;
 }
 console.log(sum(a, b)); // 3
+
+
+const app: express.Express = express()
+const port: number = 8080;
+
+// CORSの許可
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
+
+// body-parserに基づいた着信リクエストの解析
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// GetとPostのルーティング
+const router: express.Router = express.Router()
+app.get('/', (req:express.Request, res:express.Response) => {
+    res.send('Hello, world!');
+});
+app.use(router)
+
+// APIサーバ起動
+app.listen(port,()=>{ console.log('Example app listening on port 8080!') })
